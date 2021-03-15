4ff84ab9636d0284ec9750bfed57c7a8623f41e5
Kate <katherine.hayes@ucdenver.edu>
2021-03-15 15:52:16 -0600

    switched mg/ha to tons/ha

--- code/FigX_cwdtons.R
+++ code/FigX_cwdtons.R
@@ -10,5 +10,18 @@ cwd <- read.csv(here("data/CWD_output_nochar.csv"))
 
 head(cwd)
 
-ggplot(cwd, aes(x = as.factor(TREAT), y = TOTAL_MG.H)) + geom_boxplot() + 
-  labs(x = "Number of Fires", y = "Total Mg/Ha", title = "Total Mg/H")
+cwd$FINE_TONS.H <- cwd$FINE_MG.H*1.102
+cwd$X3_TONS.H <- cwd$X3.SOUND_MG.H*1.102 + cwd$X3.ROTTEN_MG.H*1.102
+cwd$TOTAL_TONS.H <- cwd$TOTAL_MG.H*1.102
+
+cwd <- cwd %>%
+  select(!c(X3.SOUND_MG.H, X3.ROTTEN_MG.H, FINE_MG.H, TOTAL_MG.H))
+
+ggplot(cwd, aes(x = as.factor(TREAT), y = TOTAL_TONS.H, fill = SITE)) + geom_boxplot() + 
+  labs(x = "Number of Fires", y = "Total Tons/Ha", title = "Total Tons/Ha")
+
+ggplot(cwd, aes(x = as.factor(TREAT), y = FINE_TONS.H, fill = SITE)) + geom_boxplot() + 
+  labs(x = "Number of Fires", y = "Total Tons/Ha", title = "Fine Tons/H")
+
+ggplot(cwd, aes(x = as.factor(TREAT), y = X3_TONS.H, fill = SITE)) + geom_boxplot() + 
+  labs(x = "Number of Fires", y = "Total Tons/Ha", title = "X3 Tons/H")
