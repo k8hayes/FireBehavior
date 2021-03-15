9096d6623c939d159e4f53b6ec4cd5b786346e95
Kate <katherine.hayes@ucdenver.edu>
2021-03-15 16:32:33 -0600

    updated figure

--- code/FigX_cwdtons.R
+++ code/FigX_cwdtons.R
@@ -6,22 +6,26 @@ theme_set(theme_cowplot())
 library(tidyverse)
 library(here)
 
-cwd <- read.csv(here("data/CWD_output_nochar.csv"))
+cwd <- read.csv(here("data/cwd_tons.h.csv"))
 
 head(cwd)
 
-cwd$FINE_TONS.H <- cwd$FINE_MG.H*1.102
-cwd$X3_TONS.H <- cwd$X3.SOUND_MG.H*1.102 + cwd$X3.ROTTEN_MG.H*1.102
-cwd$TOTAL_TONS.H <- cwd$TOTAL_MG.H*1.102
+one_plot <- ggplot(cwd, aes(x = as.factor(TREAT), y = X1HR_TONS.H, fill = SITE)) + geom_boxplot() + 
+  labs(x = "Number of Fires", y = "Tons/Ha", title = "1-Hour Fuels")+ theme(legend.position = "none")
 
-cwd <- cwd %>%
-  select(!c(X3.SOUND_MG.H, X3.ROTTEN_MG.H, FINE_MG.H, TOTAL_MG.H))
+ten_plot <- ggplot(cwd, aes(x = as.factor(TREAT), y = X10HR_TONS.H, fill = SITE)) + geom_boxplot() + 
+  labs(x = "Number of Fires", y = "Tons/Ha", title = "10-Hour Fuels") 
 
-ggplot(cwd, aes(x = as.factor(TREAT), y = TOTAL_TONS.H, fill = SITE)) + geom_boxplot() + 
-  labs(x = "Number of Fires", y = "Total Tons/Ha", title = "Total Tons/Ha")
+hund_plot <- ggplot(cwd, aes(x = as.factor(TREAT), y = X100HR_TONS.H, fill = SITE)) + geom_boxplot() + 
+  labs(x = "Number of Fires", y = "Tons/Ha", title = "100-Hour Fuels") + theme(legend.position = "none")
 
-ggplot(cwd, aes(x = as.factor(TREAT), y = FINE_TONS.H, fill = SITE)) + geom_boxplot() + 
-  labs(x = "Number of Fires", y = "Total Tons/Ha", title = "Fine Tons/H")
+thou_plot <- ggplot(cwd, aes(x = as.factor(TREAT), y = X1000HR_TONS.H, fill = SITE)) + geom_boxplot() + 
+  labs(x = "Number of Fires", y = "Tons/Ha", title = "1000-Hour Fuels")
 
-ggplot(cwd, aes(x = as.factor(TREAT), y = X3_TONS.H, fill = SITE)) + geom_boxplot() + 
-  labs(x = "Number of Fires", y = "Total Tons/Ha", title = "X3 Tons/H")
+top <- plot_grid(ncol = 2, nrow = 1, one_plot, ten_plot, rel_widths = c(1,1.25),
+                 labels = c("A.", "B."))
+top
+bottom <- plot_grid(ncol = 2, nrow = 1, hund_plot, thou_plot, rel_widths = c(1,1.25),
+                    labels = c("C.", "D."))
+
+plot_grid(top, bottom, nrow = 2, ncol = 1)
