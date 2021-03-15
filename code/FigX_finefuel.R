b3dd859046f1b0c2a20b9e1d87cfd75f115b023e
Kate <katherine.hayes@ucdenver.edu>
2021-03-15 17:35:02 -0600

    grouped by line

--- code/FigX_finefuel.R
+++ code/FigX_finefuel.R
@@ -8,9 +8,27 @@ library(here)
 
 fine <- read.csv(here("data/FFL_raw.csv"))
 
+# fine[fine$HEIGHT > 300,]
+# fine[fine$HEIGHT > 200,]
+
 ggplot(fine, aes(x = as.factor(TREAT), y = HEIGHT, fill = SITE)) + 
   geom_boxplot() + 
-  labs(x = "Number of Fires", y = "Height (cm)", title = "Fine Fuel Loads") + 
+  labs(x = "Number of Fires", y = "Height (cm)", title = "Fine Fuel Heights") + 
   scale_fill_manual(values = c("#d8b365", "#5ab4ac"),
                     labels = c("Upland", "Lowland"),
                     name = "Site")
+
+test <- fine %>%
+  group_by(SITE, TREAT, PLOT, LINE) %>%
+  summarise(AV_height = mean(HEIGHT))
+
+ ggplot(test, aes(x = as.factor(TREAT), y = AV_height, fill = SITE)) + 
+  geom_boxplot() + 
+  labs(x = "Number of Fires", y = "Height (cm)", title = "Fine Fuel Heights") + 
+  scale_fill_manual(values = c("#d8b365", "#5ab4ac"),
+                    labels = c("Upland", "Lowland"),
+                    name = "Site")
+
+ summary(fine$HEIGHT)
+
+ 
