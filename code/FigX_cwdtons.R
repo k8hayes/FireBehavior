fae02391e00d61567e96a028f8f85a942ff6731d
Kate <katherine.hayes@ucdenver.edu>
2021-03-20 12:52:35 -0600

    graphing interval on x-axis

--- code/FigX_cwdtons.R
+++ code/FigX_cwdtons.R
@@ -10,17 +10,31 @@ cwd <- read.csv(here("data/cwd_tons.h.csv"))
 
 head(cwd)
 
-one_plot <- ggplot(cwd, aes(x = as.factor(TREAT), y = X1HR_TONS.H, fill = SITE)) + geom_boxplot() + 
-  labs(x = "Number of Fires", y = "Tons/Ha", title = "1-Hour Fuels")+ theme(legend.position = "none")
+# testing interval
 
-ten_plot <- ggplot(cwd, aes(x = as.factor(TREAT), y = X10HR_TONS.H, fill = SITE)) + geom_boxplot() + 
-  labs(x = "Number of Fires", y = "Tons/Ha", title = "10-Hour Fuels") 
+one_plot <- ggplot(cwd, aes(x = Interval, y = X1HR_TONS.H, col = SITE))  + geom_jitter(width = 4) +
+  labs(x = "Years since initial fire", y = "Tons/Ha", title = "1-Hour Fuels") + 
+  scale_color_manual(name = "Site", 
+                     values = c("#feb24c","#0570b0"),
+                     labels = c("Upland", "Lowland")) + theme(legend.position = "none")
 
-hund_plot <- ggplot(cwd, aes(x = as.factor(TREAT), y = X100HR_TONS.H, fill = SITE)) + geom_boxplot() + 
-  labs(x = "Number of Fires", y = "Tons/Ha", title = "100-Hour Fuels") + theme(legend.position = "none")
+ten_plot <- ggplot(cwd, aes(x = Interval, y = X10HR_TONS.H, col = SITE))  + geom_jitter(width = 4) +
+  labs(x = "Years since initial fire", y = "Tons/Ha", title = "10-Hour Fuels") + 
+  scale_color_manual(name = "Site", 
+                     values = c("#feb24c","#0570b0"),
+                     labels = c("Upland", "Lowland"))
 
-thou_plot <- ggplot(cwd, aes(x = as.factor(TREAT), y = X1000HR_TONS.H, fill = SITE)) + geom_boxplot() + 
-  labs(x = "Number of Fires", y = "Tons/Ha", title = "1000-Hour Fuels")
+hund_plot <- ggplot(cwd, aes(x = Interval, y = X100HR_TONS.H, col = SITE))  + geom_jitter(width = 4) +
+  labs(x = "Years since initial fire", y = "Tons/Ha", title = "100-Hour Fuels") + theme(legend.position = "none") +
+  scale_color_manual(name = "Site", 
+                     values = c("#feb24c","#0570b0"),
+                     labels = c("Upland", "Lowland"))
+
+thou_plot <- ggplot(cwd, aes(x = Interval, y = X1000HR_TONS.H, col = SITE))  + geom_jitter(width = 4) +
+  labs(x = "Years since initial fire", y = "Tons/Ha", title = "1000-Hour Fuels") + 
+  scale_color_manual(name = "Site", 
+                     values = c("#feb24c","#0570b0"),
+                     labels = c("Upland", "Lowland"))
 
 top <- plot_grid(ncol = 2, nrow = 1, one_plot, ten_plot, rel_widths = c(1,1.25),
                  labels = c("A.", "B."))
