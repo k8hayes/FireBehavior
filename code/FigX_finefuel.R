8873e6f398cc4d36fa87a2a29d1491fabbed3f2d
Kate <katherine.hayes@ucdenver.edu>
2021-04-02 09:52:26 -0600

    fixed axis label

--- code/FigX_finefuel.R
+++ code/FigX_finefuel.R
@@ -8,9 +8,6 @@ library(here)
 
 fine <- read.csv(here("data/FFL_raw.csv"))
 
-# fine[fine$HEIGHT > 300,]
-# fine[fine$HEIGHT > 200,]
-
 ggplot(fine, aes(x = as.factor(TREAT), y = HEIGHT, fill = SITE)) + 
   geom_boxplot() + 
   labs(x = "Number of Fires", y = "Height (cm)", title = "Fine Fuel Heights") + 
@@ -30,14 +27,6 @@ int <- read.csv(here("data/cwd_tons.h.csv"))
 int <- int %>%
   select(c("PLOT", "Interval"))
 
-int$PLOT %in% test$PLOT
-
-test$PLOT[test$PLOT == "32_2  "] <- "32_2"
-
-plot.names <- test$PLOT
-
-plot.int <- 0
-
 for(i in 1:length(plot.names)) {
   plot.int[i] <- int$Interval[int$PLOT == plot.names[i]]
 }
@@ -45,9 +34,10 @@ plot.int
 
 test$Interval <- plot.int
 
-ggplot(test, aes(x = Interval, y = AV_height, col = SITE)) + 
+ggplot(test, aes(x = Interval,
+                 y = AV_height, col = SITE)) + 
   geom_jitter(width = 4) + 
-  labs(x = "Number of Fires", y = "Height (cm)", title = "Fine Fuel Heights") + 
+  labs(x = "Years since initial fire", y = "Height (cm)", title = "Fine Fuel Heights") + 
   scale_color_manual(values = c("#d8b365", "#5ab4ac"),
                     labels = c("Upland", "Lowland"),
                     name = "Site")
