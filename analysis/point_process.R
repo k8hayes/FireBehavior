65ddfebc6487daa744cefca573843705f8c95290
Kate <katherine.hayes@ucdenver.edu>
2021-09-16 14:15:23 -0600

    organizing

--- analysis/point_process.R
+++ analysis/point_process.R
@@ -1,60 +1,77 @@
 # Point process models
 
+# Set up ##########################
 library(tidyverse)
 library(truncnorm)
-library(randomizr)
+library(randomizr) # https://cran.r-project.org/web/packages/randomizr/vignettes/randomizr_vignette.html
 
-# generating point cloud  
-x <- round(runif(119, 0, 20))
-y <- round(runif(119, 0, 20))
-
-plot(x,y)
-
-loc <- cbind(x,y)
-loc <- as.data.frame(loc)
-
-# informing marks
-
-dbh <- read.csv(here("data/dbh_clumpsas1.csv"))
-
+# Points #########################
   # mean number of trees per plot
   meantree1 <- dbh %>% 
     filter(TREAT == 1) %>%
     group_by(PLOT) %>%
     summarise(n = n())
-  round(mean(meantree1$n)) # 119.2
-  
-  # average number per species within plot
-  meantreespp1 <- dbh %>% 
-    filter(TREAT == 1) %>%
-    group_by(PLOT, SPP) %>%
-    summarise(n = n()) %>%
-      group_by(SPP) %>%
-      summarise(AV = round(mean(n)))
-  meantreespp1$prop <- round(meantreespp1$AV / 128,
-                             digits = 2)
-  sum(meantreespp1$prop)
-  
- loc$SPP <- simple_ra(N = 119, 
-            prob_each = meantreespp1$prop,
-            conditions = meantreespp1$SPP)
+  round(mean(meantree1$n)) # 119.2 # so 119
 
-  # what's the average height per species within 1x burns
-  height <- dbh %>% 
-    filter(TREAT == 1) %>%
-    filter(HEIGHT_M != "NA") %>%
-    group_by(SPP) %>%
-    summarise(AV = mean(HEIGHT_M), SD = sd(HEIGHT_M)) 
+  # generating point cloud  
+  x <- round(runif(119, 0, 20))
+  y <- round(runif(119, 0, 20))
+
+  plot(x,y)
+
+  loc <- cbind(x,y)
+  loc <- as.data.frame(loc)
+
+# Marks ####################
+
+dbh <- read.csv(here("data/dbh_clumpsas1.csv"))
+
+  # Species ###############################
+    # average number per species within plot
+    meantreespp1 <- dbh %>% 
+      filter(TREAT == 1) %>% # only 1x burns right now
+      group_by(PLOT, SPP) %>% # group by plot / spp
+      summarise(n = n()) %>% # counts number of spp within plot
+        group_by(SPP) %>% # groups by spp only
+        summarise(AV = round(mean(n))) # average of count
+    meantreespp1$prob <- round(meantreespp1$AV / 128,# can't use 119 # doesn't add to 1
+                               digits = 2)
+    sum(meantreespp1$prob) # checking if it adds to 1
+    
+   loc$SPP <- simple_ra(N = 119, 
+              prob_each = meantreespp1$prob,
+              conditions = meantreespp1$SPP) # names
+ 
+ # Live / Dead #############################
+   meanCan1 <- dbh %>% 
+     filter(TREAT == 1) %>% # only 1x burns right now
+     group_by(PLOT, SPP) %>% # group by plot / spp
+     summarise(n = n()) %>% # counts number of spp within plot
+     group_by(SPP) %>% # groups by spp only
+     summarise(AV = round(mean(n))) # average of count
+
   
-  # why is pime height NA
-  dbh1x <- dbh %>%
-    filter(TREAT == 1)
-  dbh1x[which(is.na(dbh1x$HEIGHT_M)),]
+  # Height ##################################   
+   # what's the average height per species within 1x burns
+    height <- dbh %>% 
+      filter(TREAT == 1) %>%
+      filter(HEIGHT_M != "NA") %>% # one NA in PIME # need to check
+      group_by(SPP) %>%
+      summarise(AV = mean(HEIGHT_M), SD = sd(HEIGHT_M)) 
   
-  dbh1x[dbh1x$SPP == "PIME",]
+      # why is pime height NA
+      dbh1x <- dbh %>%
+        filter(TREAT == 1)
+      dbh1x[which(is.na(dbh1x$HEIGHT_M)),]
+      
+      dbh1x[dbh1x$SPP == "PIME",]
   
-  loc$HEIGHT <- "NA"
-  
+  # Assigning heights ################3
+      # initializing column
+        loc$HEIGHT <- 0
+      
+  loc$HEIGHT[loc$SPP=="BENE"] <- rtruncnorm(119,a = 0, mean = height$AV[height$SPP == "BENE"],
+                                                  sd = height$SD[height$SPP == "BENE"])
   loc$HEIGHT[loc$SPP=="POTR"] <- rtruncnorm(119, a = 0, mean = height$AV[height$SPP == "POTR"],
                                        sd = height$SD[height$SPP == "POTR"])
   loc$HEIGHT[loc$SPP=="PIME"] <- rtruncnorm(119, a = 0, mean = height$AV[height$SPP == "PIME"],
@@ -63,13 +80,10 @@ dbh <- read.csv(here("data/dbh_clumpsas1.csv"))
                                        sd = height$SD[height$SPP == "ALCR"])
   loc$HEIGHT[loc$SPP=="ARCTO"] <- rtruncnorm(119, a = 0,mean = height$AV[height$SPP == "ARCTO"],
                                        sd = height$SD[height$SPP == "ARCTO"])
-  loc$HEIGHT[loc$SPP=="BENE"] <- rtruncnorm(119,a = 0, mean = height$AV[height$SPP == "BENE"],
-                                        sd = height$SD[height$SPP == "BENE"])
   loc$HEIGHT[loc$SPP=="SALIX"] <- rtruncnorm(119, a = 0, mean = height$AV[height$SPP == "SALIX"],
                                        sd = height$SD[height$SPP == "SALIX"])
-
-  loc$HEIGHT <- as.numeric(loc$HEIGHT)
   loc$HEIGHT <- round(loc$HEIGHT, digits = 2)
   hist(loc$HEIGHT)
 
+  # Next, need to assign biomass
 
