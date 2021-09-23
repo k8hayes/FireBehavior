2e38e2abef56ee4f7fddecd58d245b3a1799e356
Kate <katherine.hayes@ucdenver.edu>
2021-09-23 10:56:38 -0600

    assigned biomass / adding live/dead labels

--- analysis/point_process.R
+++ analysis/point_process.R
@@ -3,15 +3,18 @@
 # Set up ##########################
 library(tidyverse)
 library(truncnorm)
+library(here)
 library(randomizr) # https://cran.r-project.org/web/packages/randomizr/vignettes/randomizr_vignette.html
+
+dbh <- read.csv(here("data/dbh_clumpsas1.csv"))
 
 # Points #########################
   # mean number of trees per plot
-  meantree1 <- dbh %>% 
-    filter(TREAT == 1) %>%
-    group_by(PLOT) %>%
-    summarise(n = n())
-  round(mean(meantree1$n)) # 119.2 # so 119
+    meantree1 <- dbh %>%
+      filter(TREAT == 1) %>%
+      group_by(PLOT) %>%
+      summarise(n = n())
+    round(mean(meantree1$n)) # 119
 
   # generating point cloud  
   x <- round(runif(119, 0, 20))
@@ -24,9 +27,7 @@ library(randomizr) # https://cran.r-project.org/web/packages/randomizr/vignettes
 
 # Marks ####################
 
-dbh <- read.csv(here("data/dbh_clumpsas1.csv"))
-
-  # Species ###############################
+  ## Species ###############################
     # average number per species within plot
     meantreespp1 <- dbh %>% 
       filter(TREAT == 1) %>% # only 1x burns right now
@@ -34,24 +35,66 @@ dbh <- read.csv(here("data/dbh_clumpsas1.csv"))
       summarise(n = n()) %>% # counts number of spp within plot
         group_by(SPP) %>% # groups by spp only
         summarise(AV = round(mean(n))) # average of count
-    meantreespp1$prob <- round(meantreespp1$AV / 128,# can't use 119 # doesn't add to 1
-                               digits = 2)
+    meantreespp1$prob <- meantreespp1$AV / sum(meantreespp1$AV)# can't use 119 # doesn't add to 1
     sum(meantreespp1$prob) # checking if it adds to 1
     
-   loc$SPP <- simple_ra(N = 119, 
+   loc$SPP <- simple_ra(N = length(loc$y), 
               prob_each = meantreespp1$prob,
               conditions = meantreespp1$SPP) # names
  
- # Live / Dead #############################
+ ## Live / Dead #############################
    meanCan1 <- dbh %>% 
      filter(TREAT == 1) %>% # only 1x burns right now
+     filter(CANOPY > 0 ) %>%
      group_by(PLOT, SPP) %>% # group by plot / spp
      summarise(n = n()) %>% # counts number of spp within plot
      group_by(SPP) %>% # groups by spp only
-     summarise(AV = round(mean(n))) # average of count
-
+     summarise(AV = round(mean(n))) 
+   meanCan1$prob <- round(meanCan1$AV / sum(meanCan1$AV), digits = 2)
+   
+  pime_loc <- loc %>% 
+    filter(SPP == "PIME") 
+  pime_loc$LIVE_DEAD <- simple_ra(N = nrow(pime_loc),
+                             prob_each = c(meanCan1$prob[meanCan1$SPP == "PIME"],
+                                           1 - meanCan1$prob[meanCan1$SPP == "PIME"]),
+                            conditions = c(1,0))
+  bene_loc <- loc %>% 
+    filter(SPP == "BENE") 
+  bene_loc$LIVE_DEAD <- simple_ra(N = nrow(bene_loc),
+                                  prob_each = c(meanCan1$prob[meanCan1$SPP == "BENE"],
+                                                1 - meanCan1$prob[meanCan1$SPP == "BENE"]),
+                                  conditions = c(1,0))
+  potr_loc <- loc %>% 
+    filter(SPP == "POTR") 
+  potr_loc$LIVE_DEAD <- simple_ra(N = nrow(potr_loc),
+                                  prob_each = c(meanCan1$prob[meanCan1$SPP == "POTR"],
+                                                1 - meanCan1$prob[meanCan1$SPP == "POTR"]),
+                                  conditions = c(1,0))
+  alcr_loc <- loc %>% 
+    filter(SPP == "ALCR") 
+  alcr_loc$LIVE_DEAD <- simple_ra(N = nrow(alcr_loc),
+                                  prob_each = c(meanCan1$prob[meanCan1$SPP == "ALCR"],
+                                                1 - meanCan1$prob[meanCan1$SPP == "ALCR"]),
+                                  conditions = c(1,0))
+  salix_loc <- loc %>% 
+    filter(SPP == "SALIX") 
+  salix_loc$LIVE_DEAD <- simple_ra(N = nrow(salix_loc),
+                                  prob_each = c(meanCan1$prob[meanCan1$SPP == "SALIX"],
+                                                1 - meanCan1$prob[meanCan1$SPP == "SALIX"]),
+                                  conditions = c(1,0))
+  arcto_loc <- loc %>% 
+    filter(SPP == "ARCTO") 
+  arcto_loc$LIVE_DEAD <- simple_ra(N = nrow(arcto_loc),
+                                  prob_each = c(meanCan1$prob[meanCan1$SPP == "ARCTO"],
+                                                1 - meanCan1$prob[meanCan1$SPP == "ARCTO"]),
+                                  conditions = c(1,0))
+  loc <- rbind(pime_loc, bene_loc, potr_loc, salix_loc, arcto_loc, alcr_loc)
   
-  # Height ##################################   
+  
+  
+  
+  
+  ## Height ##################################   
    # what's the average height per species within 1x burns
     height <- dbh %>% 
       filter(TREAT == 1) %>%
@@ -60,13 +103,13 @@ dbh <- read.csv(here("data/dbh_clumpsas1.csv"))
       summarise(AV = mean(HEIGHT_M), SD = sd(HEIGHT_M)) 
   
       # why is pime height NA
-      dbh1x <- dbh %>%
-        filter(TREAT == 1)
-      dbh1x[which(is.na(dbh1x$HEIGHT_M)),]
+      # dbh1x <- dbh %>%
+      #   filter(TREAT == 1)
+      # dbh1x[which(is.na(dbh1x$HEIGHT_M)),]
+      # 
+      # dbh1x[dbh1x$SPP == "PIME",]
       
-      dbh1x[dbh1x$SPP == "PIME",]
-  
-  # Assigning heights ################3
+  ### Assigning heights ################
       # initializing column
         loc$HEIGHT <- 0
       
@@ -85,5 +128,56 @@ dbh <- read.csv(here("data/dbh_clumpsas1.csv"))
   loc$HEIGHT <- round(loc$HEIGHT, digits = 2)
   hist(loc$HEIGHT)
 
-  # Next, need to assign biomass
+## Biomass #########################
+  ### Stem ###############################
+  dbh$STEM_BIOMASS <- 0
+  dbh$STEM_BIOMASS[dbh$SPP == "PIME"] <-  117.91*(dbh$DBH[dbh$SPP == "PIME"]^1.99) # Alexander et al. 2012
+  dbh$STEM_BIOMASS[dbh$SPP == "POTR"] <-  64.01*(dbh$DBH[dbh$SPP == "POTR"]^2.51) # Alexander et al. 2012
+  dbh$STEM_BIOMASS[dbh$SPP == "BENE"] <-  147.96*(dbh$DBH[dbh$SPP == "BENE"]^2.25) # Alexander et al. 2012
+  dbh$STEM_BIOMASS[dbh$SPP == "ALCR"] <-  exp(4.5 + 2.3*log(dbh$DBH[dbh$SPP == "ALCR"])) # Binkley et al. 1984
+  dbh$STEM_BIOMASS[dbh$SPP == "SALIX"] <- 10^(2.167 + 1.03*log10(dbh$DBH[dbh$SPP == "SALIX"])) # Bond-Lamberty et al. 2002
+  
+  biomass_stem <- dbh %>% 
+    filter(TREAT == 1) %>%
+    group_by(SPP) %>%
+    summarise(AV = mean(STEM_BIOMASS), SD = sd(STEM_BIOMASS)) 
+  
+  loc$STEM_BIOMASS[loc$SPP=="BENE"] <- rtruncnorm(119,a = 0, mean = biomass_stem$AV[height$SPP == "BENE"],
+                                            sd = biomass_stem$SD[height$SPP == "BENE"])
+  loc$STEM_BIOMASS[loc$SPP=="POTR"] <- rtruncnorm(119, a = 0, mean = biomass_stem$AV[biomass_stem$SPP == "POTR"],
+                                            sd = biomass_stem$SD[biomass_stem$SPP == "POTR"])
+  loc$STEM_BIOMASS[loc$SPP=="PIME"] <- rtruncnorm(119, a = 0, mean = biomass_stem$AV[biomass_stem$SPP == "PIME"],
+                                            sd = biomass_stem$SD[biomass_stem$SPP == "PIME"])
+  loc$STEM_BIOMASS[loc$SPP=="ALCR"] <- rtruncnorm(119, a =0, mean = biomass_stem$AV[biomass_stem$SPP == "ALCR"],
+                                            sd = biomass_stem$SD[biomass_stem$SPP == "ALCR"])
+  #loc$STEM_BIOMASS[loc$SPP=="ARCTO"] <- rtruncnorm(119, a = 0,mean = biomass_stem$AV[biomass_stem$SPP == "ARCTO"],
+                                            # sd = biomass_stem$SD[biomass_stem$SPP == "ARCTO"])
+  loc$STEM_BIOMASS[loc$SPP=="SALIX"] <- rtruncnorm(119, a = 0, mean = biomass_stem$AV[biomass_stem$SPP == "SALIX"],
+                                             sd = biomass_stem$SD[biomass_stem$SPP == "SALIX"])
+  ### Foliage ####################
+  dbh$FOL_BIOMASS <- 0
+  dbh$FOL_BIOMASS[dbh$SPP == "PIME"] <-  55.4*(dbh$DBH[dbh$SPP == "PIME"]^1.47) # Alexander et al. 2012
+  dbh$FOL_BIOMASS[dbh$SPP == "POTR"] <-  18.98*(dbh$DBH[dbh$SPP == "POTR"]^1.53) # Alexander et al. 2012
+  dbh$FOL_BIOMASS[dbh$SPP == "BENE"] <-  6.39*(dbh$DBH[dbh$SPP == "BENE"]^2.1) # Alexander et al. 2012
+  dbh$FOL_BIOMASS[dbh$SPP == "ALCR"] <-  10^(1.82 + 2.38*log10(dbh$DBH[dbh$SPP == "ALCR"])) # Binkley et al. 1984
+  dbh$FOL_BIOMASS[dbh$SPP == "SALIX"] <- 10^(2.023 + 1.065*log10(dbh$DBH[dbh$SPP == "SALIX"])) # Bond-Lamberty et al. 2002
+   
+  biomass_fol <- dbh %>% 
+    filter(TREAT == 1) %>%
+    group_by(SPP) %>%
+    summarise(AV = mean(FOL_BIOMASS), SD = sd(FOL_BIOMASS)) 
+  
+  loc$FOL_BIOMASS[loc$SPP=="BENE"] <- rtruncnorm(119,a = 0, mean = biomass_fol$AV[height$SPP == "BENE"],
+                                                  sd = biomass_fol$SD[height$SPP == "BENE"])
+  loc$FOL_BIOMASS[loc$SPP=="POTR"] <- rtruncnorm(119, a = 0, mean = biomass_fol$AV[biomass_fol$SPP == "POTR"],
+                                                  sd = biomass_fol$SD[biomass_fol$SPP == "POTR"])
+  loc$FOL_BIOMASS[loc$SPP=="PIME"] <- rtruncnorm(119, a = 0, mean = biomass_fol$AV[biomass_fol$SPP == "PIME"],
+                                                  sd = biomass_fol$SD[biomass_fol$SPP == "PIME"])
+  loc$FOL_BIOMASS[loc$SPP=="ALCR"] <- rtruncnorm(119, a =0, mean = biomass_fol$AV[biomass_fol$SPP == "ALCR"],
+                                                  sd = biomass_fol$SD[biomass_fol$SPP == "ALCR"])
+  #loc$FOL_BIOMASS[loc$SPP=="ARCTO"] <- rtruncnorm(119, a = 0,mean = biomass_fol$AV[biomass_fol$SPP == "ARCTO"],
+  # sd = biomass_fol$SD[biomass_fol$SPP == "ARCTO"])
+  loc$FOL_BIOMASS[loc$SPP=="SALIX"] <- rtruncnorm(119, a = 0, mean = biomass_fol$AV[biomass_fol$SPP == "SALIX"],
+                                                   sd = biomass_fol$SD[biomass_fol$SPP == "SALIX"])
 
+   
