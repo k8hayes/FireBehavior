9cdd351a69e2391f767eba0419943459cd2f608e
Kate <katherine.hayes@ucdenver.edu>
2021-09-23 11:34:15 -0600

    ordered correctly

--- code/adding dead spruce.R
+++ code/adding dead spruce.R
@@ -1,29 +1,29 @@
 # adding dead spruce to 2019 file
 
 library(here)
+library(allometree)
 library(tidyverse)
+
+# 2019 data ########################################
 
 herb2019 <- read.csv(here("data/dbh_clumpsas1.csv"))
 
-which(is.na(herb2019$CANOPY))
+which(is.na(herb2019$CANOPY)) # one canopy with missing entry
 herb2019[3200,]
 
-# checking which plots have dead pime entered
-pime2019 <- herb2019 %>%
-  filter(SPP== "PIME") %>%
-  filter(HEIGHT_M >0) %>%
-  filter(! is.na(CANOPY)) # one un-entered canopy from a spruce # 33_1
+    # checking which plots have dead pime entered
+    pime2019 <- herb2019 %>%
+      filter(SPP== "PIME") %>%
+      filter(HEIGHT_M >0) %>%
+      filter(! is.na(CANOPY)) # one un-entered canopy from a spruce # 33_1
 
-unique(pime2019$PLOT)
-
-pime2019dead <- pime2019[pime2019$CANOPY == 0,]
-plots2019 <- unique(pime2019dead$PLOT) # dead spruce in 8 plots
+unique(pime2019$PLOT) # checking plots
 
 # adding quadrant / exp_fact into herb2019 data
 herb2019$QUAD <- 2
 herb2019$EXP_FACT <- 50
 
-# bringing in 2018
+# 2018 data ######################################
 
 dbh2018 <- read.csv(here("data/dbh2018.csv"))
 
@@ -32,35 +32,6 @@ pime2018dead <- dbh2018 %>%
   filter(CANOPY == 0)
 
 unique(pime2018dead$PLOT)
-
-# adding height to 2018 spruce
-
-
-test <- ss_modelselect_multi(pime2019,
-                             species = "SPP",
-                             response = "HEIGHT_M",
-                             predictor = "DBH_CM")
-test
-
-testpred <- ss_simulate(ref_table = test$ss_models_info,
-                         models = test$ss_models,
-                         extrapolate = c(0,0.5))
-
-add2018$HEIGHT_M <- exp(1.308392 + 1.451135*log(log(add2018$DBH_CM + 1)))
-
-ggplot() +
-  geom_point(data = pime2019, aes(x = DBH_CM, y = HEIGHT_M)) +
-  geom_ribbon(data = testpred, aes(x = predictor, ymin = lwr, ymax = upr), 
-              alpha = 0.1) + 
-  geom_line(data = testpred, aes(x = predictor, y = fit, lty = extrapolated)) +
-  coord_cartesian(ylim = c(0, max(pime2019$HEIGHT_M)),
-                  xlim = c(0, max(testpred$predictor))) + 
-  geom_point(data = add2018, aes(x = DBH_CM, y = HEIGHT_M), col = "red")
-
-test$ss_models_info
-
-data(eqns_info, package = "allometree")
-eqns_info
 
 add2018 <- pime2018dead %>%
   filter(PLOT != "32_2") %>%
@@ -74,11 +45,43 @@ add2018 <- pime2018dead %>%
   rename("DBH_CM" = "DBH", "STEM_TYPE" = "CLUMP") %>%
   select(!c(UND, BROWSE, START_END))
 
-add2018$HEIGHT_M <- NA
 add2018$CLUMP_ID <- NA
+
+## height dbh equation ##############################
+
+test <- ss_modelselect_multi(pime2019,
+                             species = "SPP",
+                             response = "HEIGHT_M",
+                             predictor = "DBH_CM")
+test
+
+data(eqns_info, package = "allometree") # equation forms
+eqns_info
+
+testpred <- ss_simulate(ref_table = test$ss_models_info,
+                         models = test$ss_models,
+                         extrapolate = c(0,0.5))
+
+test$ss_models_info
+
+add2018$HEIGHT_M <- exp(1.308392 + 1.451135*log(log(add2018$DBH_CM + 1)))
+
+ggplot() +
+  geom_point(data = pime2019, aes(x = DBH_CM, y = HEIGHT_M)) +
+  geom_ribbon(data = testpred, aes(x = predictor, ymin = lwr, ymax = upr), 
+              alpha = 0.1) + 
+  geom_line(data = testpred, aes(x = predictor, y = fit, lty = extrapolated)) +
+  coord_cartesian(ylim = c(0, max(pime2019$HEIGHT_M)),
+                  xlim = c(0, max(testpred$predictor))) + 
+  geom_point(data = add2018, aes(x = DBH_CM, y = HEIGHT_M), col = "red") # checking added data
+
+
+# checking files
 colnames(add2018)
 colnames(herb2019)
 
+# Combining ############################################3
 test <- rbind(herb2019, add2018)
 
+write.csv(test, here("data/dbh_clumpsas1.csv"), row.names = FALSE)
 
