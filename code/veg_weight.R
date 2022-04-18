7b85ba21f3d35569cfc68dd355e8ccc0afff6930
Kate <katherine.hayes@ucdenver.edu>
2022-04-18 12:11:09 -0600

    pulling out veg weights for model

--- code/veg_weight.R
+++ code/veg_weight.R
@@ -11,7 +11,19 @@ veg_weight <- read.csv(here("data/understory_veg_weight_raw.csv"))
 
 veg_weight$DIFF <-  veg_weight$WET_WEIGHT - veg_weight$DRY_WEIGHT / veg_weight$WET_WEIGHT
 
-ggplot(veg_weight, aes(y = DIFF, fill = SITE)) + geom_boxplot() + 
+# averages for model
+veg_weight <- drop_na(veg_weight)
+
+veg_weight$moist <-  veg_weight$WET_WEIGHT - veg_weight$DRY_WEIGHT 
+
+
+veg_weight %>%
+  group_by(TREAT) %>%
+  summarise(AV = mean(moist), SD = sd(moist))
+
+
+# Plots ##########################################3
+ggplot(veg_weight, aes(y = DIFF, x = as.factor(TREAT), fill = SITE)) + geom_boxplot() + 
   labs(y = "Normalized weight lost", title = "Difference in fuel moisture between sites", x = "Sites")
 
 veg_weight %>%
@@ -21,7 +33,5 @@ veg_weight %>%
                      values = c("red", "blue", "red", "green")) + 
   labs(y = "Normalized weight lost", x = "Plot ID", 
        title = "Difference in fuel moisture across plots")
-
-
 
 
