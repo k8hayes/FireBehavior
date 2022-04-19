eda6ff40aa86b7d7f8f2f6c5ed0a36c7a6975160
Kate <katherine.hayes@ucdenver.edu>
2022-04-19 15:31:54 -0600

    updating and plotting

--- code/FigX_finefuel.R
+++ code/FigX_finefuel.R
@@ -1,19 +1,31 @@
 # graphing fine fuel heights
 
-library(ggplot2)
+library(tidyverse)
 library(cowplot)
 theme_set(theme_cowplot())
-library(tidyverse)
 library(here)
 
-fine <- read.csv(here("data/FFL_raw.csv"))
+fine <- read.csv(here("data/FF heights/FFL_raw.csv"))
 
+# plots ######################################
 ggplot(fine, aes(x = as.factor(TREAT), y = HEIGHT, fill = SITE)) + 
   geom_boxplot() + 
   labs(x = "Number of Fires", y = "Height (cm)", title = "Fine Fuel Heights") + 
   scale_fill_manual(values = c("#d8b365", "#5ab4ac"),
                     labels = c("Upland", "Lowland"),
                     name = "Site")
+
+fine_plot <- fine %>%
+  group_by(SITE, TREAT, PLOT, LINE) %>%
+  summarise(AV = mean(HEIGHT))
+
+fine_plot %>%
+  filter(TREAT != 0) %>%
+  ggplot(aes(x = as.factor(TREAT), y = AV)) + 
+  geom_boxplot(fill = "#EB984E")  + panel_border() + 
+  labs(x = "Number of Fires", y = "Height (cm)", 
+       title = "Connectivity of fine fuels") 
+
 # taking out NAs
 fine <- fine %>%
   filter(HEIGHT != "NA")
@@ -22,25 +34,27 @@ test <- fine %>%
   group_by(SITE, TREAT, PLOT, LINE) %>%
   summarise(AV_height = mean(HEIGHT))
 
-# bringing in interval
-int <- read.csv(here("data/cwd_tons.h.csv"))
-int <- int %>%
-  select(c("PLOT", "Interval"))
+# # bringing in interval
+# int <- read.csv(here("data/cwd_tons.h.csv"))
+# int <- int %>%
+#   select(c("PLOT", "Interval"))
+# 
+# for(i in 1:length(plot.names)) {
+#   plot.int[i] <- int$Interval[int$PLOT == plot.names[i]]
+# }
+# plot.int
+# 
+# test$Interval <- plot.int
+# 
+# ggplot(test, aes(x = Interval,
+#                  y = AV_height, col = SITE)) + 
+#   geom_jitter(width = 4) + 
+#   labs(x = "Years since initial fire", y = "Height (cm)", title = "Fine Fuel Heights") + 
+#   scale_color_manual(values = c("#d8b365", "#5ab4ac"),
+#                     labels = c("Upland", "Lowland"),
+#                     name = "Site")
 
-for(i in 1:length(plot.names)) {
-  plot.int[i] <- int$Interval[int$PLOT == plot.names[i]]
-}
-plot.int
-
-test$Interval <- plot.int
-
-ggplot(test, aes(x = Interval,
-                 y = AV_height, col = SITE)) + 
-  geom_jitter(width = 4) + 
-  labs(x = "Years since initial fire", y = "Height (cm)", title = "Fine Fuel Heights") + 
-  scale_color_manual(values = c("#d8b365", "#5ab4ac"),
-                    labels = c("Upland", "Lowland"),
-                    name = "Site")
-
-
- 
+# averages for model #########################
+fine %>%
+  group_by(TREAT) %>%
+  summarise(AV = mean(HEIGHT), SD = sd(HEIGHT))
