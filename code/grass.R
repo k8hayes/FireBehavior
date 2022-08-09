7325a9d27e3e2ab5e72c936602157f2ef1531bb8
Kate <katherine.hayes@ucdenver.edu>
2022-08-08 21:02:01 -0600

    plotting grass % cover

--- code/grass.R
+++ code/grass.R
@@ -8,6 +8,10 @@ theme_set(theme_cowplot())
 
 grass <- read.csv(here("data/gram_cover.csv"))
 
-
-ggplot(grass, aes(x = factor(TREATMENT), y = GRAM, fill = SITE)) + geom_boxplot() + 
-  labs(x = "Number of Fires", y = "% Grass Cover", title = "Grass Cover")
+grass %>%
+  filter(TREATMENT !=0) %>%
+  ggplot(aes(x = factor(TREATMENT), y = GRAM, fill = SITE)) + geom_boxplot() + 
+  labs(x = "Number of Fires", y = "% Grass Cover", title = "Grass Cover")+
+  scale_fill_manual(values = c("#d8b365", "#5ab4ac"),
+                    labels = c("Upland", "Lowland"),
+                    name = "Site")
